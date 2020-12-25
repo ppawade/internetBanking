d83ec5cfc1206cdc6239af162329b768e2560e72
Prachi Pawade <prachi.p.pawade@gmail.com>
2020-12-25 18:26:41 +0530

    Updated code1

--- src/test/java/com/visionit/internetBanking/testCases/BasicWay.java
+++ src/test/java/com/visionit/internetBanking/testCases/BasicWay.java
@@ -11,8 +11,25 @@ public class BasicWay {
 		WebDriver dr = new ChromeDriver();
 		dr.get("http://demo.guru99.com/v4/");
 		dr.findElement(By.name("uid")).sendKeys("mngr301183");
-	dr.findElement(By.name("password")).sendKeys("unageta");
-dr.findElement(By.name("btnLogin")).click();
+		dr.findElement(By.name("password")).sendKeys("unageta");
+		dr.findElement(By.name("btnLogin")).click();
+		
+		dr.findElement(By.xpath("//a[contains(text(),'New Customer')]")).click();
+		
+		dr.findElement(By.name("name")).sendKeys("ABC");
+		dr.findElement(By.xpath("//input[@value='f']")).click();
+		dr.findElement(By.id("dob")).sendKeys("25122020");
+		dr.findElement(By.name("addr")).sendKeys("The address mentioned");
+		dr.findElement(By.name("city")).sendKeys("Pune");
+		dr.findElement(By.name("state")).sendKeys("Maharashtra");
+		dr.findElement(By.name("pinno")).sendKeys("442001");
+		
+		dr.findElement(By.name("telephoneno")).sendKeys("987654321");
+		dr.findElement(By.name("emailid")).sendKeys("ABCPQR@gmail.com");
+		dr.findElement(By.name("password")).sendKeys("ABC@123");
+		dr.findElement(By.name("sub")).click();
+		
+
 	}
-	
+
 }
